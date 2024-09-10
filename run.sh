887468597ed197319dd513c9e1cfd7d396015e23
Rapuskin <47694129+Rapuskin@users.noreply.github.com>
2024-09-10 20:25:14 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-echo "Running the script!"
-./hello-server
+chmod +x ./linux/hello-server/*
+
+./linux/hello-server &
+
+sleep 5
+
+for LOGIN in Homer Bart Maggie; do
+    echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
+done
